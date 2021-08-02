401876e05281fddb43031ab5a94101dac1a8d9b4
Nikita Naumenko <naumenkoniki@gmail.com>
2021-08-02 20:02:51 +0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7a6402501a25c11adc99d1ff71e8d836'),
+    },
+  },
+});
